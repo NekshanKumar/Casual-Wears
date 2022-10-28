297dcfc70eee4c6ace83d72f35cf629c98ad092c
NekshanKumar <niksrajput63@gmail.com>
2022-10-28 19:27:32 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# This is a Nextjs Tailwind E-Commerce website like Amazon and Flipcart
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+#
+```
+
+1. Get the layout of the appliction ready
+
+2. List Products
+   a. add data.js
+   b. add images
+   c. render products
---
+++ components/utils/data.js
@@ -0,0 +1,77 @@
+const data = {
+  products: [
+    {
+      name: 't-Shirt',
+      slug: 'free-shirt',
+      category: 'T-Shirts',
+      image: '/images/shirt1.jpg',
+      brand: 'Comic Sense',
+      rating: 4.5,
+      numReviews: 7,
+      countInStock: 35,
+      description: 'Anime T-shirt of character Itachi Uchiha',
+    },
+
+    {
+      name: 't-Shirt',
+      slug: 'free-shirt',
+      category: 'T-Shirts',
+      image: '/images/shirt2.jpg',
+      brand: 'Comic Sense',
+      rating: 4.5,
+      numReviews: 7,
+      countInStock: 35,
+      description: 'Anime T-shirt of character Monkey D Luffy',
+    },
+
+    {
+      name: 't-Shirt',
+      slug: 'free-shirt',
+      category: 'T-Shirts',
+      image: '/images/shirt3.jpg',
+      brand: 'Comic Sense',
+      rating: 4.5,
+      numReviews: 7,
+      countInStock: 35,
+      description: 'Anime T-shirt of character Naruto Uzumaki',
+    },
+
+    {
+      name: 't-Shirt',
+      slug: 'free-shirt',
+      category: 'T-Shirts',
+      image: '/images/shirt4.jpg',
+      brand: 'Comic Sense',
+      rating: 4.5,
+      numReviews: 7,
+      countInStock: 40,
+      description: 'Anime T-shirt of character Sasuke Uchiha',
+    },
+
+    {
+      name: 't-Shirt',
+      slug: 'free-shirt',
+      category: 'T-Shirts',
+      image: '/images/shirt5.jpg',
+      brand: 'Comic Sense',
+      rating: 4.5,
+      numReviews: 7,
+      countInStock: 25,
+      description: 'Anime T-shirt of character Madara Uchiha',
+    },
+
+    {
+      name: 't-Shirt',
+      slug: 'free-shirt',
+      category: 'T-Shirts',
+      image: '/images/shirt6.jpg',
+      brand: 'Comic Sense',
+      rating: 4.5,
+      numReviews: 7,
+      countInStock: 35,
+      description: 'Anime T-shirt of character Roronoa Zoro',
+    },
+  ],
+};
+
+export default data;
