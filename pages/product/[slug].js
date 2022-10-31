941a0f57bb46e53a04d0f26456453a00f9188302
NekshanKumar <niksrajput63@gmail.com>
2022-10-31 18:08:15 +0530

    Day-5 Updates

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,33 +1,33 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
 import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
   const { state, dispatch } = useContext(Store);
+
+  const router = useRouter();
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
   if (!product) {
     return <div>Produt Not Found</div>;
   }
-
   const addToCartHandler = () => {
-    const existItem = state.cart.cartItems.find((x) => x.slug === slug);
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
     const quantity = existItem ? existItem.quantity + 1 : 1;
     if (product.countInStock < quantity) {
-      alert('Sorry, desired product is out of stock');
+      alert('Sorry. Product is out of stock');
       return;
     }
-    dispatch({
-      type: 'CART_ADD_ITEM',
-      payload: { ...product, quantity },
-    });
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
   };
+
   return (
     <Layout title={product.name}>
       <div className="py-2">
@@ -60,7 +60,7 @@ export default function ProductScreen() {
           <div className="card p-5">
             <div className="mb-2 flex justify-between">
               <div>Price</div>
-              <div>₹{product.price}</div>
+              <div>${product.price}</div>
             </div>
             <div className="mb-2 flex justify-between">
               <div>Status</div>
